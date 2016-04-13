bce9c90ba3d068e0308d2d9d0d13106bce4b7396
Guillaume Chau <guillaume.b.chau@gmail.com>
2016-04-13 09:51:59 +0200

    Home page help improvements

--- app/modules/todos/client/components/Home.vue
+++ app/modules/todos/client/components/Home.vue
@@ -8,6 +8,7 @@
         <p>
             This is a kickstart project built with tasty technologies like <a href="https://www.meteor.com/">meteor</a> (the javascript stack), <a href="https://vuejs.org/">vue</a> (a fast &amp; powerfull ui library), <a href="https://webpack.github.io/">webpack</a> (a bundler with code-splitting), <a href="https://github.com/kadirahq/flow-router">flow-router</a> (a modern meteor router), <a href="https://babeljs.io/">babel</a> (you can write next gen javascript), and it features a lazy-loaded module-based architecture.
         </p>
+        <hr />
         <p>
             Update your app dependencies:
         </p>
@@ -27,6 +28,7 @@
         <p class="console">
             meteor add &lt;package-name&gt;
         </p>
+        <hr />
         <p>
             Change the main style:
         </p>
@@ -51,11 +53,21 @@
         <p class="dir">
             <span>app</span>&#47;<span>main</span>&#47;<span>client</span>&#47;<span>components</span>&#47;<span>NotFound.vue</span>
         </p>
+        <hr />
         <p>
             Add modules here (copy todos module folder):
         </p>
         <p class="dir">
             <span>app</span>&#47;<span>modules</span>&#47;<span>your-module</span>
+        </p>
+        <p class="warning">
+            All your client-specific files must be under a client folder for the hot-reloading to work (or else, the server may restart unexpectedly). See <a href="https://github.com/thereactivestack/meteor-webpack/issues/71">#71</a>.
+        </p>
+        <p>
+            For example:
+        </p>
+        <p class="dir">
+            <span>app</span>&#47;<span>modules</span>&#47;<span>your-module</span>&#47;<span><b>client</b></span>&#47;<span>components</span>&#47;<span>YourComponent.vue</span>
         </p>
         <p>
             Don't forget to include the module routes here:
@@ -63,6 +75,12 @@
         <p class="dir">
             <span>app</span>&#47;<span>main</span>&#47;<span>client</span>&#47;<span>routes.js</span>
         </p>
+        <p class="console">
+            import 'modules/todos/client/routes';<br />
+            import 'modules/admin/client/routes';<br />
+            import 'modules/your-module/client/routes';
+        </p>
+        <hr />
         <p>
             To launch the app on mobile platforms, edit this file and change your computer local network ip address:
         </p>
@@ -72,6 +90,7 @@
         <p>
             Then follow the official guide <a href="http://guide.meteor.com/mobile.html">here</a>.
         </p>
+        <hr />
         <p>
             Please share your thoughts, report issues and comment this kickstart project <a href="https://github.com/Akryum/kickstart-meteor-vue-huge-app/issues">on Github</a>!
         </p>
